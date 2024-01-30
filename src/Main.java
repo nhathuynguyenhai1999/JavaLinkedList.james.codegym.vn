d049f24cbf9406ca1c8c268b7eebc111816e7170
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-30 14:18:50 +0700

    update my files

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,25 @@ public class Main {
         clonedList.printList();
 
         System.out.println("Checking if the list contains 20: " + myList.contains(20));
+        CircularQueue circularQueue = new CircularQueue();
+
+        // Thêm phần tử vào hàng đợi
+        circularQueue.enQueue(1);
+        circularQueue.enQueue(2);
+        circularQueue.enQueue(3);
+
+        // Hiển thị hàng đợi
+        System.out.print("Hàng đợi sau khi thêm phần tử: ");
+        circularQueue.displayQueue();
+
+        // Lấy ra một phần tử
+        Integer removedValue = circularQueue.deQueue();
+        if (removedValue != null) {
+            System.out.println("Phần tử đã bị lấy ra: " + removedValue);
+        }
+
+        // Hiển thị hàng đợi sau khi lấy ra phần tử
+        System.out.print("Hàng đợi sau khi lấy ra phần tử: ");
+        circularQueue.displayQueue();
     }
 }
